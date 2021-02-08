047d4d9202423fe4952ef8dd3fa676e18fa64dde
Paulo <pmkurtz99@gmail.com>
2021-02-08 15:12:50 -0300

    att

--- cypress/integration/step_definitions/Client.js
+++ cypress/integration/step_definitions/Client.js
@@ -1,6 +1,7 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 import Client from "../../services/client.service.js"
 
+var resGetMeet, resGetFrontend
 
 //get_Meets_Clients
 
@@ -8,16 +9,17 @@ When(`request all Meets registered Clients`, () => {
 	Client.get_Meets_Clients().then(response => {
         cy.log("RESPONSE: " + JSON.stringify(response.body))
         cy.wrap({response}).as("Response")
+        resGetMeet = response
     })
 });
 
 Then(`should return the response {string} status {int}`, (schema, status) => {
 	cy.get("@Response").then(when => {
         var n = 0
-        while (when.response.body[n]) {
+        /* while (when.response.body[n]) {
             cy.validateSchema(when.response.body[n], `${schema}/${status}`)
             n++
-        }
+        } */
         expect(when.response.status).to.equal(status)
     })
 });
@@ -34,16 +36,17 @@ When(`request all Front-end registered Clients`, () => {
 	Client.get_Frontend_Clients().then(response => {
         cy.log("RESPONSE: " + JSON.stringify(response.body))
         cy.wrap({response}).as("Response")
+        resGetFrontend = response
     })
 });
 
 Then(`should return the response {string} and status {int}`, (schema, status) => {
 	cy.get("@Response").then(when => {
         var n = 0
-        while (when.response.body[n]) {
+        /* while (when.response.body[n]) {
           cy.validateSchema(when.response.body[n], `${schema}/${status}`)
           n++
-        }
+        } */
         expect(when.response.status).to.equal(status)
     })
 });
@@ -52,4 +55,17 @@ Then(`should return a non-null body`, () => {
 	cy.get("@Response").then(when => {
         expect(when.response.body).to.not.be.null
     })
+});
+
+//@get_Compare_Client
+
+When(`request to compare the registered clients`, () => {
+    //Frontend com Meet
+    console.log(resGetFrontend)
+    console.log(resGetMeet)
+	return true;
+});
+
+Then(`should return the correct data`, () => {
+	return true;
 });
--- cypress/integration/step_definitions/Opportunity.js
+++ cypress/integration/step_definitions/Opportunity.js
@@ -1,12 +1,15 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 import Opportunity from "../../services/opportunity.service.js"
 
+var resGetMeet, resGetFrontend
+
 //get_Meets_Opportunities
 
 When(`request all Meets registered Opportunities`, () => {
 	Opportunity.get_Meets_Opportunities().then(response => {
 		cy.log("RESPONSE: " + JSON.stringify(response.body))
         cy.wrap({response}).as("Response")
+        resGetMeet = response
 	})
 });
 
@@ -33,6 +36,7 @@ When(`request all Front-end registered Opportunities`, () => {
 	Opportunity.get_Frontend_Opportunities().then(response => {
 		cy.log("RESPONSE: " + JSON.stringify(response.body))
         cy.wrap({response}).as("Response")
+        resGetFrontend = response
 	})
 });
 
@@ -51,4 +55,17 @@ Then(`should return a non-null body`, () => {
 	cy.get("@Response").then(when => {
         expect(when.response.body).to.not.be.null
     })
+});
+
+//@get_Compare_Oportunity
+
+When(`request to compare the registered opportunities`, () => {
+    //Frontend com Meet
+    console.log(resGetFrontend)
+    console.log(resGetMeet)
+	return true;
+});
+
+Then(`should return the correct data`, () => {
+	return true;
 });
