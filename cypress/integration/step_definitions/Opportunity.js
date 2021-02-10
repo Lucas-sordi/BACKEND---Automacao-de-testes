429c14ef9c7cb002fef0103804e9523fbaa4f4d8
Paulo <pmkurtz99@gmail.com>
2021-02-10 16:21:47 -0300

    juncao Oportunidade

--- cypress/integration/step_definitions/Opportunity.js
+++ cypress/integration/step_definitions/Opportunity.js
@@ -9,7 +9,7 @@ When(`request all Meets registered Opportunities`, () => {
 	Opportunity.get_Meets_Opportunities().then(response => {
 		cy.log("RESPONSE: " + JSON.stringify(response.body))
         cy.wrap({response}).as("Response")
-        Response_Meets = response
+        Response_Meets = response.body
 	})
 });
 
@@ -36,7 +36,7 @@ When(`request all Front-end registered Opportunities`, () => {
 	Opportunity.get_Frontend_Opportunities().then(response => {
 		cy.log("RESPONSE: " + JSON.stringify(response.body))
         cy.wrap({response}).as("Response")
-        Response_Front = response
+        Response_Front = response.body
 	})
 });
 
@@ -60,8 +60,39 @@ Then(`should return a non-null body`, () => {
 //@get_Compare_Oportunity
 
 When(`request to compare the registered opportunities`, () => {
-    //Frontend com Meet
-	return true;
+    Response_Meets.forEach(element => {
+        delete element._id
+    });
+    Response_Front.forEach(element => {
+        delete element._id
+    });
+    var nameList = []
+    for (let i = 0; i < Response_Front.length; i++) {
+        for (let j = 0; j < Response_Meets.length; j++) {
+            if (Response_Front[i].nome === Response_Meets[j].nome) {
+                nameList.push(Response_Front[i].nome)
+                cy.log("Oportunidade: " + Response_Front[i].nome).then(() => {
+                    if (JSON.stringify(Response_Front[i]) == JSON.stringify(Response_Meets[j])) {
+                        expect(JSON.stringify(Response_Front[i])).to.eq(JSON.stringify(Response_Meets[j]))
+                    } else {
+                        for (var eachProperty in Response_Front[i]) {
+                            try {
+                                expect(Response_Front[i][eachProperty]).to.eq(Response_Meets[j][eachProperty])
+                            } catch (err) { continue }
+                        }
+                    }
+                })
+            }
+        }
+    }
+    var opportunityErrorList = Response_Front.filter(e => !nameList.includes(e.nome))
+    opportunityErrorList.forEach(element => {
+        cy.log("Oportunidade: " + element.nome).then(() => {
+            try {
+                expect(Response_Meets).to.include(JSON.stringify(element))
+            } catch (err) { return }
+        })
+    });
 });
 
 Then(`should return the correct data`, () => {
